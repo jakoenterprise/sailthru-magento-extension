c2a7fb5bb919acd01f864f11a379c5b7b4e9484a
santosh1989 <santosh@elivepages.com>
2016-06-06 21:10:54 +0530

    fix: skip out of stock products for content api

--- app/code/local/Sailthru/Email/Model/Observer/Content.php
+++ app/code/local/Sailthru/Email/Model/Observer/Content.php
@@ -40,8 +40,11 @@ class Sailthru_Email_Model_Observer_Content extends Sailthru_Email_Model_Abstrac
        if($this->_isEnabled) {
             try{
                 $product = $observer->getEvent()->getProduct();
-                if($product->getStatus()=='2') return $this;
-                $response = Mage::getModel('sailthruemail/client_content')->saveProduct($product);
+               if($product->getTypeId()=='configurable' && $product->getStatus()=='1' && $this->isSaleable($product->getId())){
+                	$response = Mage::getModel('sailthruemail/client_content')->saveProduct($product);
+               }else{
+               	 	return $this;
+               }
             } catch (Exception $e) {
                 Mage::logException($e);
             }
@@ -49,4 +52,17 @@ class Sailthru_Email_Model_Observer_Content extends Sailthru_Email_Model_Abstrac
         return $this;
     }
 
+    
+    public function isSaleable($id)
+    {
+		$isSaleable		= false;
+		$originalStore  = Mage::app()->getStore(); // save the original store setting 
+		Mage::app()->setCurrentStore('default'); //switch to the default store
+		$_product	= Mage::getModel("catalog/product")->load($id);	
+		$isSaleable = $_product->isSaleable();
+		Mage::app()->setCurrentStore($originalStore->getId()); // switch back to the original
+		unset($_product);
+		return $isSaleable;
+    }    
+    
 }
